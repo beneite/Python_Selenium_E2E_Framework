4cb677667fccdfe90096b1ec64044a3fe2c8b9db
beneite <im.ashish707@gmail.com>
2026-05-17 11:48:31 +0530

    adding html report with screeshots

---
+++ README.md
@@ -0,0 +1,2 @@
+pytest -n 3 -m smoke --browser firefox --html=report.html --self-contained-html
+pytest -n=3 -m=smoke --browser=firefox --html=report.html --self-contained-html
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,21 +1,26 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from pages.base_page import BasePage
 
-class LoginPage:
+class LoginPage(BasePage):
 
     customer_email_input = (By.XPATH, "//input[@id = 'customer_email']")
     customer_password_input = (By.XPATH, "//input[@id = 'customer_password']")
     sign_in_button = (By.XPATH, "//input[@value = 'Sign In']")
 
     def __init__(self, driver):
+        super().__init__(driver)
         self.driver = driver
 
     def enter_customer_email(self, customer_email):
         element: WebElement = self.driver.find_element(*self.customer_email_input)
+        element.clear()
         element.send_keys(customer_email)
+        print("useremail:", element.get_attribute("value"))
 
     def enter_customer_password(self, customer_password):
         element: WebElement = self.driver.find_element(*self.customer_password_input)
+        element.clear()
         element.send_keys(customer_password)
 
     def click_sign_in(self):
